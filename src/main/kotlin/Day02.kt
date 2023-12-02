dd69dc856a03351b9a8b04bfe3d40e0658f2dc3a
johannesrave <johannes.rave@gmail.com>
2023-12-02 22:20:48 +0100

    Day02 inspired variant

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,47 +1,56 @@
 import kotlin.system.measureTimeMillis
 
-
 fun main() {
     val durationA = measureTimeMillis { println("Solution for Day02a: ${Day02a("input/02.txt").solve()}") }
-    println("solution took $durationA milliseconds")
+    println("Solving Day02a took $durationA milliseconds")
 
     val durationB = measureTimeMillis { println("Solution for Day02b: ${Day02b("input/02.txt").solve()}") }
-    println("solution took $durationB milliseconds")
+    println("Solving Day02b took $durationB milliseconds")
 }
 
 class Day02a(inputFileName: String) : Day(inputFileName) {
-    private val cubesPerColor = mapOf("red" to 12, "green" to 13, "blue" to 14)
     private val gameIdPattern = """Game (\d+): .*""".toRegex()
 
-    fun solve(): Int = input.split("\n")
-        .sumOf { it ->
-            cubesPerColor.keys.forEach { color ->
-                val allHandsInGame = getCubeColorPattern(color).findAll(it)
-                allHandsInGame.forEach {
-                    val cubesOfColorInHand = it.groupValues[1].toInt()
-                    if (cubesOfColorInHand > cubesPerColor[color]!!) return@sumOf 0
-                }
+    fun solve(): Int = input
+        .split("\n")
+        .sumOf { gameRound ->
+            Color.entries.forEach { color ->
+                if (!color.allOccurrencesArePossible(gameRound)) return@sumOf 0
             }
-            val (_, gameIdString) = gameIdPattern.find(it)!!.groupValues
+            val (_, gameIdString) = gameIdPattern.find(gameRound)!!.groupValues
             val gameId = gameIdString.toInt()
             gameId
         }
 
-    private fun getCubeColorPattern(color: String) = """(\d+) $color""".toRegex()
+    enum class Color(color: String, private val maxCubes: Int) {
+        R("red", 12), G("green", 13), B("blue", 14);
+
+        private val colorOccurrenceRegex = """(\d+) $color""".toRegex()
+
+        fun allOccurrencesArePossible(round: String) = colorOccurrenceRegex
+            .findAll(round)
+            .map { match -> match.groupValues[1].toInt() }
+            .all { amountOfCubes -> (amountOfCubes <= maxCubes) }
+    }
 }
 
 
 class Day02b(inputFileName: String) : Day(inputFileName) {
-    private val cubesPerColor = mapOf("red" to 12, "green" to 13, "blue" to 14)
-
-    fun solve(): Int = input.split("\n")
-        .sumOf { it ->
-            val productOfMaxCubesPerColor = cubesPerColor.keys.map { color ->
-                val allHandsInGame = getColorRegex(color).findAll(it)
-                allHandsInGame.maxOf { it.groupValues[1].toInt() }
-            }.fold(1) { product, maxCubesPerColor -> product * maxCubesPerColor }
-            return@sumOf productOfMaxCubesPerColor
+    fun solve(): Int = input
+        .split("\n")
+        .sumOf { round ->
+            return@sumOf Color.entries
+                .map { color -> color.findMaxOccurrenceInRound(round) }
+                .reduce { product, maxCubesPerColor -> product * maxCubesPerColor }
         }
 
-    private fun getColorRegex(color: String) = """(\d+) $color""".toRegex()
+    enum class Color(color: String) {
+        R("red"), G("green"), B("blue");
+
+        private val colorOccurrenceRegex = """(\d+) $color""".toRegex()
+
+        fun findMaxOccurrenceInRound(round: String) = colorOccurrenceRegex
+            .findAll(round)
+            .maxOf { match -> match.groupValues[1].toInt() }
+    }
 }
