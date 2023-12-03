1817fa2b81c86aa89c0e3bb4be9293f25327410b
johannesrave <johannes.rave@gmail.com>
2023-12-03 23:10:47 +0100

    Day03 bikeshedding

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -9,9 +9,9 @@ fun main() {
     println("solution took $durationB milliseconds")
 }
 
-class Day03a(inputFileName: String) : Day(inputFileName) {
+open class Day03a(inputFileName: String) : Day(inputFileName) {
 
-    fun solve(): Int {
+    open fun solve(): Int {
         val symbolPositions = parsePositionsByPattern(input, """[^0-9.]""")
         val partNumbers = parsePartNumbers(input)
 
@@ -21,7 +21,7 @@ class Day03a(inputFileName: String) : Day(inputFileName) {
             .sumOf { it.number }
     }
 
-    private fun adjacentPartNumbers(pos: Point2D, partNumbers: Array<Array<PartNumber?>>): Set<PartNumber> {
+    fun adjacentPartNumbers(pos: Point2D, partNumbers: Array<Array<PartNumber?>>): Set<PartNumber> {
         val (x, y) = pos
         return (y - 1..y + 1).flatMap { _y ->
             (x - 1..x + 1).mapNotNull { _x ->
@@ -30,7 +30,7 @@ class Day03a(inputFileName: String) : Day(inputFileName) {
         }.toSet()
     }
 
-    private fun parsePositionsByPattern(input: String, pattern: String): List<Point2D> {
+    fun parsePositionsByPattern(input: String, pattern: String): List<Point2D> {
         return input
             .split("\n")
             .flatMapIndexed { y, row ->
@@ -40,7 +40,7 @@ class Day03a(inputFileName: String) : Day(inputFileName) {
             }
     }
 
-    private fun parsePartNumbers(input: String): Array<Array<PartNumber?>> {
+    fun parsePartNumbers(input: String): Array<Array<PartNumber?>> {
         val rows = input.split("\n")
         val board = Array(rows.size) { Array<PartNumber?>(rows.first().length) { null } }
 
@@ -58,55 +58,16 @@ class Day03a(inputFileName: String) : Day(inputFileName) {
     data class PartNumber(val number: Int, val xRange: IntRange, val y: Int)
 
     data class Point2D(val x: Int, val y: Int)
-
 }
 
-class Day03b(inputFileName: String) : Day(inputFileName) {
-    fun solve(): Int {
+class Day03b(inputFileName: String) : Day03a(inputFileName) {
+    override fun solve(): Int {
         val gearCandidates = parsePositionsByPattern(input, "\\*")
         val partNumbers = parsePartNumbers(input)
 
         return gearCandidates
             .map { pos -> adjacentPartNumbers(pos, partNumbers) }
-            .filter { partNumbers_ -> partNumbers_.size == 2 }
-            .sumOf { partNumbers_ -> partNumbers_.fold(1 as Int) { acc, number -> acc * number.number } }
+            .filter { adjacentPartNumbers -> adjacentPartNumbers.size == 2 }
+            .sumOf { adjacentPartNumbers -> adjacentPartNumbers.fold(1 as Int) { acc, number -> acc * number.number } }
     }
-
-    private fun adjacentPartNumbers(pos: Point2D, partNumbers: Array<Array<PartNumber?>>): Set<PartNumber> {
-        val (x, y) = pos
-        return (y - 1..y + 1).flatMap { _y ->
-            (x - 1..x + 1).mapNotNull { _x ->
-                partNumbers[_y][_x]
-            }
-        }.toSet()
-    }
-
-    private fun parsePositionsByPattern(input: String, pattern: String): List<Point2D> {
-        return input
-            .split("\n")
-            .flatMapIndexed { y, row ->
-                pattern.toRegex()
-                    .findAll(row)
-                    .map { result -> Point2D(result.range.first, y) }
-            }
-    }
-
-    private fun parsePartNumbers(input: String): Array<Array<PartNumber?>> {
-        val rows = input.split("\n")
-        val board = Array(rows.size) { Array<PartNumber?>(rows.first().length) { null } }
-
-        rows.forEachIndexed { y, row ->
-            "\\d+".toRegex()
-                .findAll(row)
-                .forEach { result ->
-                    val partNumber = PartNumber(result.value.toInt(), result.range, y)
-                    partNumber.xRange.forEach { x -> board[y][x] = partNumber }
-                }
-        }
-        return board
-    }
-
-    data class PartNumber(val number: Int, val xRange: IntRange, val y: Int)
-
-    data class Point2D(val x: Int, val y: Int)
 }
