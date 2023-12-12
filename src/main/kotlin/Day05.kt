8cf12df2827fea66d94658d50ae94733a83e3d15
johannesrave <johannes.rave@gmail.com>
2023-12-12 23:06:14 +0100

    Day05b refactoring for clarity wip

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -17,7 +17,7 @@ open class Day05a(inputFileName: String) : Day(inputFileName) {
         return horticulturalMappings
             .fold(seeds) { items, mapping ->
                 items.map { item ->
-                    mapping.criterionRanges.find { item in it.range }
+                    mapping.transformations.find { item in it.range }
                         ?.let { item + it.offset } ?: item
                 }
             }.min()
@@ -31,40 +31,42 @@ open class Day05a(inputFileName: String) : Day(inputFileName) {
         .map { block ->
             val lines = block.split("\n")
             val name = lines[0].split(" ")[0]
-            val criterionRanges = lines.drop(1)
+            val transformations = lines.drop(1)
                 .map { it.split(" ").map { it.toLong() } }
                 .map {
                     val (destination, source, length) = it
-                    CriterionRange(Day05b.SeedRange(source, source + length - 1), destination - source)
+                    Transformation(Day05b.Range(source, source + length - 1), destination - source)
                 }.sortedBy { it.range.first }
-            HorticulturalStep(name, criterionRanges)
+            TransformationStep(name, transformations)
         }
 
-    data class CriterionRange(val range: Day05b.SeedRange, val offset: Long)
-    data class HorticulturalStep(val name: String, val criterionRanges: List<CriterionRange>)
+    data class Transformation(val range: Day05b.Range, val offset: Long)
+    data class TransformationStep(val name: String, val transformations: List<Transformation>)
 }
 
 class Day05b(inputFileName: String) : Day05a(inputFileName) {
     override fun solve(): Long {
-        val allSeedRanges = parseSeedRanges(input)
+        val seedRanges = parseSeedRanges(input)
         val horticulturalSteps = parseHorticulturalSteps(input)
-
-        /*
-        * algo:
-        * 1. iterate over horticultural steps, transforming seedRanges if necessary
-        * 2. for each criterionRange of the current step:
-        *   3. check whether the seedRange and criterionRange overlap
-        *       3a. seedRange is completely within
-        *   4. if yes, build list of transformed and/or untouched seedRanges and flatten it
-        *   5. if horticultural steps remain, go to 2.
-        * 6. find range with minimal start/lower bound and return that
-        * */
-
-        return allSeedRanges.horticulturate(horticulturalSteps).minBy { it.first }.first
+        return process(seedRanges, horticulturalSteps).minBy { it.first }.first
     }
 
-    private fun parseSeedRanges(input: String): List<SeedRange> {
-        val firstLine = input.split("\n\n").first()
+    fun process(startingRanges: List<Range>, transformationSteps: List<TransformationStep>): List<Range> =
+        transformationSteps.fold(startingRanges) { ranges, (_, transformations) ->
+            ranges.flatMap { range ->
+                val relevantTransformations = transformations.filter { range.overlaps(it.range) }
+
+                val untransformed = range - (relevantTransformations.map { it.range })
+                val transformed = relevantTransformations.map { (transformationRange, offset) ->
+                    range.intersection(transformationRange).transform(offset)
+                }
+
+                (transformed + untransformed).sortedBy { it.first }
+            }
+        }
+
+    private fun parseSeedRanges(input: String): List<Range> {
+        val (firstLine) = input.lines()
 
         val ranges = """\d+""".toRegex()
             .findAll(firstLine)
@@ -72,110 +74,45 @@ class Day05b(inputFileName: String) : Day05a(inputFileName) {
             .chunked(2)
             .toList()
             .map {
-                val (start, length) = it.first() to it.last()
-                val range = SeedRange(start, start + length - 1)
-                range
+                val (first, length) = it.first() to it.last()
+                Range(first, first + length - 1)
             }.sortedBy { it.first }
         return ranges
     }
 
+    data class Range(val first: Long, val last: Long) {
+        operator fun minus(otherRanges: List<Range>): List<Range> {
+            if (otherRanges.isEmpty()) return listOf(this)
 
-    data class SeedRange(val first: Long, val last: Long) {
-        operator fun contains(num: Long): Boolean {
-            return num >= this.first && num <= this.last
-        }
-
-        fun overlaps(otherRange: SeedRange): Boolean {
-            return !(last <= otherRange.first || first >= otherRange.last)
-        }
-
-        fun union(otherRange: SeedRange): SeedRange {
-            if (!this.overlaps(otherRange)) throw IllegalArgumentException("Can't build UNION, ranges don't overlap.")
-
-            return when {
-                first in otherRange && last in otherRange -> otherRange
-                first in otherRange && last !in otherRange -> SeedRange(otherRange.first, last)
-                first !in otherRange && last in otherRange -> SeedRange(first, otherRange.last)
-                first !in otherRange && last !in otherRange -> this
-                else -> throw IllegalStateException()
-            }
-        }
-
-        fun intersection(otherRange: SeedRange): SeedRange {
-            return when {
-                first in otherRange && last in otherRange -> this
-                first in otherRange && last !in otherRange -> SeedRange(first, otherRange.last)
-                first !in otherRange && last in otherRange -> SeedRange(otherRange.first, last)
-                first !in otherRange && last !in otherRange -> otherRange
-                else -> throw IllegalStateException()
-            }
-        }
-
-        operator fun minus(otherRange: SeedRange): List<SeedRange> {
-            return when {
-                first in otherRange && last in otherRange -> listOf()
-
-                first !in otherRange && last in otherRange -> listOf(SeedRange(first, otherRange.first - 1))
-
-                first in otherRange && last !in otherRange -> listOf(SeedRange(otherRange.last + 1, last))
-
-                first !in otherRange && last !in otherRange ->
-                    listOf(SeedRange(first, otherRange.first - 1), SeedRange(otherRange.last + 1, last))
-
-                else -> throw IllegalStateException()
-            }
-        }
-
-        fun shift(offset: Long): SeedRange = SeedRange(first + offset, last + offset)
-
-        fun minusMultiple(testCriterionRanges: List<SeedRange>): List<SeedRange> {
-            if (testCriterionRanges.isEmpty())
-                return listOf(this)
-
-            val sortedCritRanges = testCriterionRanges.sortedBy { it.first }
+            val sortedCritRanges = otherRanges.sortedBy { it.first }
 
             val result = listOfNotNull(
-                SeedRange(first, sortedCritRanges.first().first - 1).takeIf { it.first <= it.last },
-                SeedRange(sortedCritRanges.last().last + 1, last).takeIf { it.first <= it.last }
+                Range(first, sortedCritRanges.first().first - 1).takeIf { it.first <= it.last },
+                Range(sortedCritRanges.last().last + 1, last).takeIf { it.first <= it.last }
             ).toMutableList()
 
             if (sortedCritRanges.size > 1) {
-                for (index in 0..<sortedCritRanges.size-1) {
+                for (index in 0..<sortedCritRanges.size - 1) {
                     val cur = sortedCritRanges[index]
                     val next = sortedCritRanges[index + 1]
-                    result.add(SeedRange(cur.last + 1, next.first - 1))
+                    result.add(Range(cur.last + 1, next.first - 1))
                 }
             }
             return result.sortedBy { it.first }
         }
+
+        fun transform(offset: Long): Range = Range(first + offset, last + offset)
+
+        fun overlaps(otherRange: Range): Boolean = !(last <= otherRange.first || first >= otherRange.last)
+
+        fun intersection(otherRange: Range): Range = when {
+            first in otherRange && last in otherRange -> this
+            first in otherRange && last !in otherRange -> Range(first, otherRange.last)
+            first !in otherRange && last in otherRange -> Range(otherRange.first, last)
+            first !in otherRange && last !in otherRange -> otherRange
+            else -> throw IllegalStateException()
+        }
+
+        operator fun contains(num: Long): Boolean = num >= this.first && num <= this.last
     }
 }
-
-fun List<Day05b.SeedRange>.horticulturate(horticulturalSteps: List<Day05a.HorticulturalStep>): List<Day05b.SeedRange> =
-    horticulturalSteps
-        .fold(this) { seedRanges, (name,criterionRanges) ->
-//            val criterionRanges = step.criterionRanges
-            println(name)
-            return@fold seedRanges.flatMap { seedRange ->
-                val relevantCritRanges = criterionRanges.filter { seedRange.overlaps(it.range) }
-                val untransformed = seedRange.minusMultiple(relevantCritRanges.map { it.range })
-
-                val shifted = relevantCritRanges.map { (range, offset) ->
-                    seedRange.intersection(range).shift(offset)
-                }
-
-                val result = (shifted + untransformed).sortedBy { it.first }
-                println("seedRange")
-                println(seedRange)
-                println("relevantCritRanges")
-                println(relevantCritRanges)
-                println("untransformed")
-                println(untransformed)
-                println("shifted")
-                println(shifted)
-                println("result")
-                println(result)
-
-                return@flatMap result
-            }
-        }
--- src/test/kotlin/Day05Test.kt
+++ src/test/kotlin/Day05Test.kt
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
 
 class Day05aTest {
 
@@ -30,101 +29,101 @@ class Day05bSolutionTest {
 
 class Day05bUnitTest {
     @Test
-    fun `horticulturate seedRange remains unchanged`() {
-        val testRange = listOf(Day05b.SeedRange(1L, 5L))
+    fun `process seedRange remains unchanged`() {
+        val testRange = listOf(Day05b.Range(1L, 5L))
         val testSteps = listOf(
-            Day05a.HorticulturalStep("test", listOf(Day05a.CriterionRange(Day05b.SeedRange(6L, 10L), 10)))
+            Day05a.TransformationStep("test", listOf(Day05a.Transformation(Day05b.Range(6L, 10L), 10)))
         )
 
-        assertEquals(testRange, testRange.horticulturate(testSteps))
+        assertEquals(testRange.toList(), Day05b("input/05.test.txt").process(testRange, testSteps).toList())
     }
 
     @Test
-    fun `horticulturate seedRange is shifted completely`() {
-        val testRange = listOf(Day05b.SeedRange(1L, 5L))
+    fun `process seedRange is shifted completely`() {
+        val testRange = listOf(Day05b.Range(1L, 5L))
         val testSteps = listOf(
-            Day05a.HorticulturalStep("test", listOf(Day05a.CriterionRange(Day05b.SeedRange(1L, 5L), 10)))
+            Day05a.TransformationStep("test", listOf(Day05a.Transformation(Day05b.Range(1L, 5L), 10)))
         )
 
-        assertEquals(listOf(Day05b.SeedRange(11L, 15L)), testRange.horticulturate(testSteps))
+        assertEquals(listOf(Day05b.Range(11L, 15L)), Day05b("input/05.test.txt").process(testRange, testSteps))
     }
 
     @Test
-    fun `horticulturate seedRange partially shifted with two ranges remaining`() {
-        val testRange = listOf(Day05b.SeedRange(1L, 10L))
+    fun `process seedRange partially shifted with two ranges remaining`() {
+        val testRange = listOf(Day05b.Range(1L, 10L))
         val testSteps = listOf(
-            Day05a.HorticulturalStep(
+            Day05a.TransformationStep(
                 "test", listOf(
-                    Day05a.CriterionRange(Day05b.SeedRange(6L, 10L), 10)
+                    Day05a.Transformation(Day05b.Range(6L, 10L), 10)
                 )
             )
         )
 
         assertEquals(
-            listOf(Day05b.SeedRange(1L, 5L), Day05b.SeedRange(16L, 20L)),
-            testRange.horticulturate(testSteps)
+            listOf(Day05b.Range(1L, 5L), Day05b.Range(16L, 20L)),
+            Day05b("input/05.test.txt").process(testRange, testSteps)
         )
     }
 
     @Test
     fun `minusMultiple with fully contained ranges returns correct List of SeedRange`() {
-        val testSeedRange = Day05b.SeedRange(1L, 10L)
+        val testRange = Day05b.Range(1L, 10L)
         val testCriterionRanges = listOf(
-            Day05b.SeedRange(3L, 5L),
-            Day05b.SeedRange(8L, 9L),
+            Day05b.Range(3L, 5L),
+            Day05b.Range(8L, 9L),
         )
 
         assertEquals(
             listOf(
-                Day05b.SeedRange(1L, 2L),
-                Day05b.SeedRange(6L, 7L),
-                Day05b.SeedRange(10L, 10L),
+                Day05b.Range(1L, 2L),
+                Day05b.Range(6L, 7L),
+                Day05b.Range(10L, 10L),
             ),
-            testSeedRange.minusMultiple(testCriterionRanges)
+            testRange.minus(testCriterionRanges)
         )
     }
 
 
     @Test
     fun `minusMultiple with partially contained ranges returns correct List of SeedRange`() {
-        val testSeedRange = Day05b.SeedRange(3L, 8L)
+        val testRange = Day05b.Range(3L, 8L)
         val testCriterionRanges = listOf(
-            Day05b.SeedRange(1L, 5L),
-            Day05b.SeedRange(8L, 10L),
+            Day05b.Range(1L, 5L),
+            Day05b.Range(8L, 10L),
         )
 
         assertEquals(
             listOf(
-                Day05b.SeedRange(6L, 7L)
+                Day05b.Range(6L, 7L)
             ),
-            testSeedRange.minusMultiple(testCriterionRanges)
+            testRange.minus(testCriterionRanges)
         )
     }
 
 
     @Test
     fun `minusMultiple with fully enclosing single range returns empty List`() {
-        val testSeedRange = Day05b.SeedRange(3L, 8L)
+        val testRange = Day05b.Range(3L, 8L)
         val testCriterionRanges = listOf(
-            Day05b.SeedRange(1L, 10L),
+            Day05b.Range(1L, 10L),
         )
 
         assertEquals(
-            emptyList<Day05b.SeedRange>(),
-            testSeedRange.minusMultiple(testCriterionRanges)
+            emptyList<Day05b.Range>(),
+            testRange.minus(testCriterionRanges)
         )
     }
 
     @Test
     fun `minusMultiple with fully enclosing single range from testinput returns empty List`() {
-        val testSeedRange = Day05b.SeedRange(55L, 67L)
+        val testRange = Day05b.Range(55L, 67L)
         val testCriterionRanges = listOf(
-            Day05b.SeedRange(50L, 97L),
+            Day05b.Range(50L, 97L),
         )
 
         assertEquals(
-            emptyList<Day05b.SeedRange>(),
-            testSeedRange.minusMultiple(testCriterionRanges)
+            emptyList<Day05b.Range>(),
+            testRange.minus(testCriterionRanges)
         )
     }
 }
