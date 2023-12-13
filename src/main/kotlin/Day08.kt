d4548100cf1e56f64b89ffdc01ced9c5e0d0610e
johannesrave <johannes.rave@gmail.com>
2023-12-14 00:29:32 +0100

    Day08b solved and refactored

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -80,25 +80,3 @@ class Day08b(inputFileName: String) : Day08a(inputFileName) {
         }.findLCM()
     }
 }
-
-// got this from https://www.baeldung.com/kotlin/lcm
-fun findLCM(a: Long, b: Long): Long {
-    val larger = if (a > b) a else b
-    val maxLcm = a * b
-    var lcm = larger
-    while (lcm <= maxLcm) {
-        if (lcm % a == 0L && lcm % b == 0L) {
-            return lcm
-        }
-        lcm += larger
-    }
-    return maxLcm
-}
-
-fun List<Long>.findLCM(): Long {
-    var result = this[0]
-    for (i in 1..<size) {
-        result = findLCM(result, this[i])
-    }
-    return result
-}
--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,1 +1,23 @@
 fun <T> Sequence<T>.repeat() = sequence { while (true) yieldAll(this@repeat) }
+
+// got this from https://www.baeldung.com/kotlin/lcm
+fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+fun List<Long>.findLCM(): Long {
+    var result = this[0]
+    for (i in 1..<size) {
+        result = findLCM(result, this[i])
+    }
+    return result
+}
