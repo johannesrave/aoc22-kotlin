0ad6d2231ebff352bb8edb7d7f0bfc6a4e5af743
johannesrave <johannes.rave@gmail.com>
2023-12-18 23:20:50 +0100

    Day12a solved

--- src/test/kotlin/Day12Test.kt
+++ src/test/kotlin/Day12Test.kt
@@ -3,35 +3,9 @@ import org.junit.jupiter.api.Test
 
 class Day12aTest {
     @Test
-    fun `findAll`() {
-        "(1.1)".toRegex()
-            .findAll("1111")
-            .map { it.groupValues }.forEach { println(it) }
-    }
-
-    @Test
-    fun `find`() {
-        var result = "(1.1)".toRegex().find("1111")
-        while (result != null) {
-            println(result.range)
-            result = result.next()
-        }
-    }
-
-
-    @Test
     fun `findValidVariants matches more than one`() {
         val day12a = Day12a("input/12.test.txt")
         val variant = day12a.findValidVariants(".??..??...?##." to listOf(1, 1, 3))
-
-        assertEquals(4, variant.count())
-    }
-
-    @Test
-    fun `findValidVariants matches more than one 2`() {
-        val day12a = Day12a("input/12.test.txt")
-
-        val variant = day12a.findValidVariants("??..??...?##." to listOf(1, 1, 3))
 
         assertEquals(4, variant.count())
     }
@@ -43,7 +17,7 @@ class Day12aTest {
 
     @Test
     fun solveInput() {
-        assertEquals(0, Day12a("input/12.txt").solve())
+        assertEquals(7792, Day12a("input/12.txt").solve())
     }
 }
 
