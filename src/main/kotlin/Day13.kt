9f6c9727917b322b7d57a630456818580c86bf8d
johannesrave <johannes.rave@gmail.com>
2023-12-20 11:10:44 +0100

    Day13a solved

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -11,42 +11,41 @@ fun main() {
 open class Day13a(inputFileName: String) : Day(inputFileName) {
     open fun solve(): Int {
         val boards = parseBoards(input)
-        return boards.sumOf { calculateScore(it).also { println(it) } }
+        return boards.sumOf { calculateScore(it) }.also { println(it) }
     }
 
     private fun calculateScore(board: Array<CharArray>): Int {
-        val verticalCandidates = findCandidatesForSymmetry(board)
-        println("verticalCandidates: $verticalCandidates")
-
-        testForSymmetryAtIndices(verticalCandidates, board)?.let { return it * 100 }
-
         val transposedBoard = board.transpose()
+        println("board:")
+        board.onEach { println(it) }
+        println("transposedBoard:")
+        transposedBoard.onEach { println(it) }
         val horizontalCandidates = findCandidatesForSymmetry(transposedBoard)
         println("horizontalCandidates: $horizontalCandidates")
 
-        testForSymmetryAtIndices(horizontalCandidates, transposedBoard)?.let { return it }
+        testForSymmetryAtIndices(horizontalCandidates, transposedBoard).also { println(it) }?.let { return it }
 
-        return -1
+        val verticalCandidates = findCandidatesForSymmetry(board)
+        println("verticalCandidates: $verticalCandidates")
+
+        testForSymmetryAtIndices(verticalCandidates, board).also { println(it) }?.let { return it * 100 }
+        throw IllegalArgumentException()
     }
 
     private fun findCandidatesForSymmetry(board: Array<CharArray>) = (0..<board.lastIndex)
         .filter { board[it].contentEquals(board[it + 1]) }
 
-    private fun testForSymmetryAtIndices(indices: List<Int>, board: Array<CharArray>): Int? {
-        board.forEach { println(it) }
-        indices.forEach { i ->
+    private fun testForSymmetryAtIndices(indices: List<Int>, board: Array<CharArray>): Int? = indices
+        .find { i ->
             val firstHalf = board.copyOfRange(0, i + 1).reversed()
-            val secondHalf = board.copyOfRange(i + 1, board.lastIndex)
+            val secondHalf = board.copyOfRange(i + 1, board.lastIndex+1)
 
-            val isSymmetric = (firstHalf zip secondHalf)
+            (firstHalf zip secondHalf)
                 .onEach { pair ->
                     println("${pair.first.joinToString("")} | ${pair.second.joinToString("")}")
                 }
-                .all { (first, second) -> first.contentEquals(second) }
-            if (isSymmetric) return (i + 1)
-        }
-        return null
-    }
+                .all { (first, second) -> first.contentEquals(second) }.also { println(it) }
+        }?.plus(1)
 
     private fun parseBoards(input: String) = input
         .split("\n\n")
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -1,8 +1,28 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertTrue
 
 class Day13aTest {
+    @Test
+    fun transpose() {
+        val board = arrayOf(
+            arrayOf('X', 'X', 'X').toCharArray(),
+            arrayOf('O', 'O', 'O').toCharArray(),
+            arrayOf('O', 'O', 'O').toCharArray(),
+        )
+
+        val transposedBoard = arrayOf(
+            arrayOf('X', 'O', 'O').toCharArray(),
+            arrayOf('X', 'O', 'O').toCharArray(),
+            arrayOf('X', 'O', 'O').toCharArray(),
+        )
+
+        board.transpose().forEachIndexed { i, row ->
+            assertContentEquals(row, transposedBoard[i])
+        }
+    }
+
     @Test
     fun solveTestInput() {
         assertEquals(405, Day13a("input/13.test.txt").solve())
@@ -10,8 +30,7 @@ class Day13aTest {
 
     @Test
     fun solveInput() {
-        assertTrue(39526 > Day13a("input/13.txt").solve())
-        assertEquals(0, Day13a("input/13.txt").solve())
+        assertEquals(34772, Day13a("input/13.txt").solve())
     }
 }
 
