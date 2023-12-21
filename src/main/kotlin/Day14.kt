4e1e1fa7bee7a64e51bece9b1b207ded2fa916cd
johannesrave <johannes.rave@gmail.com>
2023-12-21 01:07:21 +0100

    Day14a wip with GravityOrdering

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -10,8 +10,63 @@ fun main() {
 
 open class Day14a(inputFileName: String) : Day(inputFileName) {
     open fun solve(): Int {
-        return -1
+        val rocks = parseRocks(input)
+            .also { it.forEach { println(it) } }
+            .also { println() }
+            .transpose().map { it.reversed().toCharArray() }
+        println()
+
+        return rocks.map { row ->
+            gravityOrder(row)
+        }.toTypedArray()
+            .transpose()
+            .also { it.forEach { println(it) } }
+            .also { println() }
+            .reversed()
+            .also { it.forEach { println(it) } }
+            .also { println() }
+            .mapIndexed { i, row -> (i + 1) * row.count { c -> c == 'O' } }.sum()
     }
+
+    private fun gravityOrder(row: CharArray, exc: Char = '#'): CharArray {
+        val ranges = findContinuousRanges(row, exc)
+        for ((start, end) in ranges) {
+            row.copyOfRange(start, end).sortedArray().copyInto(row, start)
+        }
+        return row
+    }
+
+    private fun findContinuousRanges(row: CharArray, exc: Char): MutableSet<Pair<Int, Int>> {
+        val ranges = mutableSetOf<Pair<Int, Int>>()
+        var begin: Int? = null
+        row.forEachIndexed { i, c ->
+            if (c != exc && begin == null) begin = i
+            if (c == exc && begin != null) {
+                ranges.add(begin!! to i)
+                begin = null
+            }
+        }
+        if (begin != null) ranges.add(begin!! to row.size)
+        return ranges
+    }
+
+    class GravityOrdering : Comparator<Char> {
+        override fun compare(a: Char?, b: Char?): Int {
+            val roundRock = 'O'
+            val squareRock = '#'
+            val sand = '.'
+            val result = when {
+                a == roundRock && b == sand -> 1
+                a == sand && b == roundRock -> -1
+                else -> 0
+            }
+                .also { println("$b $a : $it") }
+            return result
+        }
+
+    }
+
+    private fun parseRocks(input: String): Array<CharArray> = input.lines().map { it.toCharArray() }.toTypedArray()
 }
 
 class Day14b(inputFileName: String) : Day14a(inputFileName) {
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,7 +1,21 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
 
 class Day14aTest {
+    @Test
+    fun `sortedWith GravityOrdering works correctly `() {
+
+        val toSort = ".O.#......".toCharArray()
+
+        val ordering = Day14a.GravityOrdering()
+        val sorted = toSort.sortedWith(ordering).toCharArray()
+
+        val result = "..O#......".toCharArray()
+
+        assertContentEquals(result, sorted)
+    }
+
     @Test
     fun solveTestInput() {
         assertEquals(136, Day14a("input/14.test.txt").solve())
