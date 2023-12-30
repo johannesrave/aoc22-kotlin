5ba5476c707c22ccbdc2290b493d39b49086c210
johannesrave <johannes.rave@gmail.com>
2023-12-30 15:06:37 +0100

    Day17a wip -test Input solved, but probably have to switch to imperative loops instead to avoid ridiculous memory allocations

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -33,51 +33,78 @@ open class Day17a(inputFileName: String) : Day(inputFileName) {
             val newPaths = currentPath.walkAndMarkHeatLoss(board, minimalHeatLoss)
             pathsToWalk += newPaths
         }
-        minimalHeatLoss.onEach { println(it.joinToString { num -> num.toString().padStart(3) }) }
-        println()
-        finishedPaths.map { path ->
-            path.steps.withIndex().joinToString(" ") { item ->
-                val (i, step) = item
-                val (pos, dir, loss) = step
-                val heatLossUpToStep = path.steps.take(i + 1).sumOf { it.heatLoss }
-                "${dir.toString().padStart(5)} -> [${pos.x},${pos.y}], ~${heatLossUpToStep.toString().padStart(3)} |"
-            }.also {
-                println("${path.totalHeatLoss}: $it")
-                println(path.printPath(board))
-                println(println())
-            }
-        }
 
-        return minimalHeatLoss.last().last()
+//        finishedPaths
+//            .minBy { it.totalHeatLoss }
+//            .let { path ->
+//                path.steps.withIndex()
+//                    .joinToString("    \n") { item ->
+//                        val (i, step) = item
+//                        val (pos, dir, loss, heatLossUpToStep) = step
+//                        val pathUpToHere = Path(path.steps.take(i + 1).toMutableList(), heatLossUpToStep)
+//                        val lastThreeDirections = LastThreeDirections.from(pathUpToHere)
+////                        val heatLossUpToStep = path.steps.take(i + 1).sumOf { it.heatLoss }
+//
+//                        "${dir.toString().padStart(5)}" +
+//                                " -> [${pos.x},${pos.y}], " +
+//                                "~${heatLossUpToStep.toString().padStart(3)}, " +
+//                                "(${lastThreeDirections}) |"
+//                    }
+//
+//                    .also {
+//                        println("${path.totalHeatLoss}: $it")
+//                        println(path.printPath(board))
+//                        println()
+//                    }
+//            }
+//        println()
+//        minimalHeatLoss.onEach { println(it.joinToString { num -> num.toString().padStart(3) }) }
+
+        return minimalHeatLoss.last().last().minOf { it.value }
     }
 
     data class Path(val steps: MutableList<Step>, val totalHeatLoss: Int) {
-        fun walkAndMarkHeatLoss(board: Array<IntArray>, minimalHeatLoss: Array<IntArray>): List<Path> {
+        fun walkAndMarkHeatLoss(
+            board: Array<IntArray>,
+            minimalHeatLossBoard: Array<Array<MutableMap<LastThreeDirections, Int>>>
+        ): List<Path> {
             val currentStep = steps.last()
             val (pos, _, _) = currentStep
-            return getValidDirs(currentStep, board)
+            val lastThreeDirs = LastThreeDirections.from(this)
+
+            return getValidDirsToTurn()
+                .filter { board.tileExists(pos.x + it.x, pos.y + it.y) }
                 .filter { dir ->
-                    val newHeatLoss = board[pos.y + dir.y][pos.x + dir.x]
-                    (totalHeatLoss + newHeatLoss) < minimalHeatLoss[pos.y + dir.y][pos.x + dir.x]
+                    val newHeatLoss = board[pos.y + dir.y][pos.x + dir.x] + totalHeatLoss
+                    val currentMinimalHeatLossOnTile = minimalHeatLossBoard[pos.y + dir.y][pos.x + dir.x]
+                        .getOrDefault(lastThreeDirs, Int.MAX_VALUE)
+
+                    (newHeatLoss) < currentMinimalHeatLossOnTile
                 }
                 .map { dir ->
                     val newPos = Pos(pos.x + dir.x, pos.y + dir.y)
-                    val newHeatLoss = board[pos.y + dir.y][pos.x + dir.x]
-                    minimalHeatLoss[newPos.y][newPos.x] = totalHeatLoss + newHeatLoss
-                    Step(newPos, dir, newHeatLoss, totalHeatLoss + newHeatLoss)
+                    val heatLossOnTile = board[pos.y + dir.y][pos.x + dir.x]
+                    val totalHeatLossUpToStep = heatLossOnTile + totalHeatLoss
+                    minimalHeatLossBoard[newPos.y][newPos.x][lastThreeDirs] = totalHeatLossUpToStep
+
+                    Step(newPos, dir, heatLossOnTile, totalHeatLossUpToStep)
                 }.map {
-                    this.copy(steps = (steps + it).toMutableList(), totalHeatLoss = totalHeatLoss + it.heatLoss)
+                    this.copy(steps = (steps + it).toMutableList(), totalHeatLoss = it.totalHeatLossUpToStep)
                 }
         }
 
-        private fun getValidDirs(step: Step, board: Array<IntArray>): Collection<Direction> {
-            val (pos, currentDirection) = step
-            val mustTurn = steps.takeLast(3).all { it.enteredGoing == currentDirection }
-            return when {
+        private fun getValidDirsToTurn(): Collection<Direction> {
+            val (pos, currentDirection) = steps.last()
+            val lastThreeDirs = LastThreeDirections.from(this)
+            val mustTurn = lastThreeDirs.wentStraight()
+//            println("$lastThreeDirs :  $mustTurn")
+
+            val validDirections = when {
                 mustTurn -> currentDirection.turnDirections()
                 else -> currentDirection.allDirections()
             }
-                .filter { board.tileExists(pos.x + it.x, pos.y + it.y) }
+            return validDirections
+
         }
 
         fun printPath(board: Array<IntArray>): String {
@@ -100,21 +127,64 @@ open class Day17a(inputFileName: String) : Day(inputFileName) {
 
     data class Step(val pos: Pos, val enteredGoing: Direction, val heatLoss: Int, val totalHeatLossUpToStep: Int)
 
+    data class LastThreeDirections(val a: Direction, val b: Direction?, val c: Direction?) {
+        fun wentStraight(): Boolean = (a == b && b == c)
+
+        companion object {
+            fun from(path: Path): LastThreeDirections {
+                return path.steps.takeLast(3).asReversed()
+                    .let {
+                        LastThreeDirections(
+                            it[0].enteredGoing,
+                            it.getOrNull(1)?.enteredGoing,
+                            it.getOrNull(2)?.enteredGoing
+                        )
+                    }
+                return path.steps
+                    .reversed()
+                    .take(3)
+                    .map { it.enteredGoing }
+                    .let { LastThreeDirections(it[0], it.getOrNull(1), it.getOrNull(2)) }
+            }
+        }
+    }
+
     enum class Direction(val x: Int, val y: Int) {
         Up(0, -1), Right(1, 0), Down(0, 1), Left(-1, 0);
 
         fun allDirections() = when (this) {
-            Up -> setOf(Left, Up, Right)
-            Right -> setOf(Up, Right, Down)
-            Down -> setOf(Right, Down, Left)
-            Left -> setOf(Down, Left, Up)
+            Up -> Direction.allDirectionsFromUp
+            Right -> Direction.allDirectionsFromRight
+            Down -> Direction.allDirectionsFromDown
+            Left -> Direction.allDirectionsFromLeft
         }
 
-        fun turnDirections() = allDirections() - this
+        fun turnDirections() = when (this) {
+            Up -> Direction.turnDirectionsFromUp
+            Right -> Direction.turnDirectionsFromRight
+            Down -> Direction.turnDirectionsFromDown
+            Left -> Direction.turnDirectionsFromLeft
+        }
+
+        companion object {
+            val allDirectionsFromUp = setOf(Left, Up, Right)
+            val allDirectionsFromRight = setOf(Up, Right, Down)
+            val allDirectionsFromDown = setOf(Right, Down, Left)
+            val allDirectionsFromLeft = setOf(Down, Left, Up)
+
+            val turnDirectionsFromUp = setOf(Left, Right)
+            val turnDirectionsFromRight = setOf(Up, Down)
+            val turnDirectionsFromDown = setOf(Right, Left)
+            val turnDirectionsFromLeft = setOf(Down, Up)
+        }
     }
 
     private fun getMinimalHeatLossBoard(board: Array<IntArray>) =
-        Array(board.size) { IntArray(board.first().size) { 999 } }
+        Array(board.size) {
+            Array(board.first().size) {
+                mutableMapOf<LastThreeDirections, Int>()
+            }
+        }
 }
 
 class Day17b(inputFileName: String) : Day17a(inputFileName) {
